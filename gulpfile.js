379d54319cf3f4b58ba417972f9802e17c9ed304
Franz Geffke <m@f-a.nz>
2020-01-10 20:25:13 +0100

    gulp debug: 6

--- gulpfile.js
+++ gulpfile.js
@@ -1,13 +1,14 @@
-var { gulp } = require("gulp");
-var { series, src, dest } = require("gulp");
-var imagemin = require("gulp-imagemin");
+const gulp = require("gulp");
+const imagemin = require("gulp-imagemin");
 
-var config = {
+const config = {
   accessKeyId: process.env.AWS_ACCESS_KEY_ID,
   secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY
 };
 
-var s3 = require("gulp-s3-upload")(config);
+const s3 = require("gulp-s3-upload")(config);
+const { series } = require("gulp");
+const { src, dest } = require("gulp");
 
 // TODO: We should decide on either JPG or PNG
 function processImages() {
